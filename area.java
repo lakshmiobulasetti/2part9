93d81daadb193119d7e73cb891930c92562eede1
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-07 12:32:08 +0530

    Update and rename README.md to area.java

---
+++ area.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	double l=sc.nextDouble();
+	double h=sc.nextDouble();
+	double m=l*h;
+	System.out.println("%.5f" ,m);
+	}
+}
